5db93745c393b20c1718fb1dd10a70a8a9c13035
RichardFreeman <huangdezhou@ACA80168.ipt.aol.com>
2015-08-25 22:07:33 +0800

    add file stat demo

---
+++ files/stat.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+int main(int argc, char *argv[])
+{
+
+    char path[100];
+    struct stat buf;
+
+    printf("Please input your file path:\n");
+    scanf("%s", path);
+
+    // int st = lstat(path, &buf);
+    int st = stat(path, &buf);
+
+    if(st == -1)
+    {
+        printf("The path is not correct!");
+    }
+
+    printf("file size: %lld\n",  buf.st_size);
+    printf("device number: %d\n", buf.st_dev);
+    printf("link:%d\n", buf.st_nlink);
+    printf("user id:%d\n", buf.st_uid);
+    printf("group id:%d\n", buf.st_gid);
+    printf("io buffer size:%d\n", buf.st_blksize);
+    printf("file block count:%lld\n", buf.st_blocks);
+    printf("last time readed:%ld\n", buf.st_atime);
+
+    if(S_ISREG(buf.st_mode))
+        printf("file type is regular\n");
+    else if(S_ISDIR(buf.st_mode))
+        printf("file type is directory\n");
+
+    return(0);
+}
